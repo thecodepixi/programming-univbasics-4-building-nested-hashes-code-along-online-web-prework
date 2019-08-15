fb9a00aca4ad3a8ec155b1a7b30c16a61ded8a3c
Emily Harber <emily.a.harber@gmail.com>
2019-08-15 00:08:54 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,15 +5,17 @@ def second_challenge
       patriarch: {},
       matriarch: {},
       hero: {},
-      hero_friends: {}
+      hero_friends: []
       }, 
     :capulet => {
       #your key/value pairs here!
       patriarch: {},
       matriarch: {},
       heroine: {},
-      heroine_friends: {}
-      }}
+      heroine_friends: []
+      }
+    
+  }
 
   
 end
